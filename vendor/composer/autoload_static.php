e03f0bbb3965a3e624c5ecbbbb12b401c2ce2050
mohamed wassim gabtni <mohamedwassim.gabtni1@esprit.tn>
2020-02-08 15:03:39 +0100

    resolved php.ini

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -66,6 +66,7 @@ class ComposerStaticInit9482fcdba46053674edfe37eda9bdde2
         'F' => 
         array (
             'Fig\\Link\\' => 9,
+            'FOS\\UserBundle\\' => 15,
         ),
         'D' => 
         array (
@@ -199,6 +200,10 @@ class ComposerStaticInit9482fcdba46053674edfe37eda9bdde2
         'Fig\\Link\\' => 
         array (
             0 => __DIR__ . '/..' . '/fig/link-util/src',
+        ),
+        'FOS\\UserBundle\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/friendsofsymfony/user-bundle',
         ),
         'Doctrine\\Instantiator\\' => 
         array (
